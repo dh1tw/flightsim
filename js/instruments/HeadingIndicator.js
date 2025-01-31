9d4ccd41b587c177ab8c177dae15f0d9aa96f8f1
Tobias Wellnitz, DH1TW <tobias@dh1tw.de>
2025-01-31 01:31:20 +0100

    refactor: Optimize HeadingIndicator rendering and remove redundant heading text

--- js/instruments/HeadingIndicator.js
+++ js/instruments/HeadingIndicator.js
@@ -3,34 +3,34 @@ class HeadingIndicator extends Instrument {
         super(x, y, size);
         this.heading = 0;  // degrees (0-359)
     }
-    
+
     draw(ctx) {
         // Draw common background first
         this.drawInstrumentBackground(ctx);
-        
+
         ctx.save();
         ctx.translate(this.x, this.y);
-        
-        const radius = this.size/2 - 20;
-        
+
+        const radius = this.size / 2 - 5;
+
         // Draw rotating compass card
         ctx.save();
         ctx.rotate(-this.heading * Math.PI / 180);
-        
+
         // Draw degree markings and numbers
         ctx.lineWidth = 2;
         ctx.strokeStyle = this.colors.markings;
         ctx.fillStyle = this.colors.numbers;
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
-        ctx.font = `bold ${this.size/15}px Arial`;
-        
-        for(let i = 0; i < 360; i += 5) {
+        ctx.font = `bold ${this.size / 15}px Arial`;
+
+        for (let i = 0; i < 360; i += 5) {
             const angle = (i * Math.PI / 180);
             const isCardinal = i % 90 === 0;
             const isMajor = i % 30 === 0;
             const length = isCardinal ? 20 : (isMajor ? 15 : 10);
-            
+
             // Draw tick marks
             ctx.beginPath();
             ctx.moveTo(
@@ -42,25 +42,25 @@ class HeadingIndicator extends Instrument {
                 -Math.cos(angle) * radius
             );
             ctx.stroke();
-            
+
             // Draw numbers/letters
             if (isCardinal || (isMajor && !isCardinal)) {
-                const textRadius = radius - 35;
+                const textRadius = radius - 27;
                 let text;
-                
+
                 if (i === 0) text = 'N';
                 else if (i === 90) text = 'E';
                 else if (i === 180) text = 'S';
                 else if (i === 270) text = 'W';
-                else text = (i/10).toString();
-                
+                else text = (i / 10).toString();
+
                 // Make cardinal points larger and bold
                 if (isCardinal) {
-                    ctx.font = `bold ${this.size/12}px Arial`;
+                    ctx.font = `bold ${this.size / 12}px Arial`;
                 } else {
-                    ctx.font = `${this.size/15}px Arial`;
+                    ctx.font = `${this.size / 15}px Arial`;
                 }
-                
+
                 ctx.fillText(
                     text,
                     Math.sin(angle) * textRadius,
@@ -68,14 +68,14 @@ class HeadingIndicator extends Instrument {
                 );
             }
         }
-        
+
         ctx.restore();
-        
+
         // Draw aircraft symbol in center
         ctx.strokeStyle = 'red';
         ctx.fillStyle = 'red';
         ctx.lineWidth = 2;
-        
+
         // Aircraft body
         ctx.beginPath();
         ctx.moveTo(0, -20);  // Nose
@@ -84,26 +84,16 @@ class HeadingIndicator extends Instrument {
         ctx.lineTo(15, 15);  // Right wing
         ctx.closePath();
         ctx.fill();
-        
+
         // Aircraft tail
         ctx.beginPath();
         ctx.moveTo(-10, 15);
         ctx.lineTo(10, 15);
         ctx.stroke();
-        
+
         ctx.restore();
-        
-        // Draw heading number at bottom
-        ctx.font = `${this.size/12}px Arial`;
-        ctx.fillStyle = this.colors.numbers;
-        ctx.textAlign = 'center';
-        ctx.fillText(
-            Math.round(this.heading).toString().padStart(3, '0') + '°',
-            this.x,
-            this.y + radius - 10
-        );
     }
-    
+
     update(heading) {
         this.heading = heading % 360;
         if (this.heading < 0) this.heading += 360;
